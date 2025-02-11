2e29d58c98cfb1fc826bfe0ca2af8c7bc529cadd
matis <matis.taam@epitech.eu>
2025-02-11 16:11:16 +0100

    [ADD] strchr function

--- src/strchr.asm
+++ src/strchr.asm
@@ -5,10 +5,28 @@
 ;; strchr
 ;;
 
-BITS 64                                  ; Mode 64 bits
-section .note.GNU-stack noexec           ; Marque la stack comme non exécutable
+BITS 64
+section .note.GNU-stack noexec
 
-section .text                            ; Section pour le code
-    global strchr                        ; Point d'entrée du programme
+section .text
+    global strchr
 
-strchr:                                  ; Étiquette de début du programme
+strchr:
+    mov rax, rdi                         ; Sauvergarde l'addresse de la chaîne
+
+.loop:
+    cmp byte [rax], sil                  ; Compare avec le caractère recherché
+    je .found                            ; Si égal, saute à .found
+
+    cmp byte [rax], 0                    ; Vérifie si fin de chaîne
+    je .not_found                        ; Si oui, saute à .not_found
+
+    inc rax                             ; Caractère suivant
+    jmp .loop                           ; Retourne au début de la boucle
+
+.found:
+    ret
+
+.not_found:
+    xor rax, rax
+    ret
