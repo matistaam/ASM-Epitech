dfdca821900d12fa2953ed826d806997046f8b8b
matis <matis.taam@epitech.eu>
2025-02-11 19:33:26 +0100

    [ADD] memset + memcpy + strcmp functions

--- src/memcpy.asm
+++ src/memcpy.asm
@@ -5,3 +5,26 @@
 ;; memcpy
 ;;
 
+BITS 64
+section .note.GNU-stack noexec
+
+section .text
+    global memcpy
+
+memcpy:
+    mov rax, rdi                    ; Sauvegarde le pointeur destination pour le retour
+    mov rcx, rdx                    ; Met le compteur dans rcx
+
+    cmp rcx, 0                      ; Vérifie si le compteur == 0
+    je .done                        ; Si oui, termine
+
+.loop:
+    mov r8b, byte [rsi]            ; Charge un octet depuis la source
+    mov byte [rdi], r8b            ; Copie l'octet vers la destination
+    inc rsi                        ; Incrémente le pointeur source
+    inc rdi                        ; Incrémente le pointeur destination
+    dec rcx                        ; Décrémente le compteur
+    jnz .loop                      ; Continue tant que rcx != 0
+
+.done:
+    ret
--- src/memset.asm
+++ src/memset.asm
@@ -5,3 +5,26 @@
 ;; memset
 ;;
 
+BITS 64
+section .note.GNU-stack noexec
+
+section .text
+    global memset
+
+memset:
+    mov rax, rdi                         ; Sauvegarde et retourne le pointeur original
+    mov rcx, rdx                         ; Met le compteur n dans rcx
+    mov al, sil                          ; Met le caractère de remplissage dans al
+
+    cmp rcx, 0                          ; Vérifie si n == 0
+    je .done                            ; Si oui, termine
+
+.loop:
+    mov byte [rdi], sil                 ; Écrit le caractère à l'adresse pointée
+    inc rdi                             ; Incrémente le pointeur
+    dec rcx                             ; Décrémente le compteur
+    jnz .loop                           ; Continue tant que rcx != 0
+
+.done:
+    mov rax, rdi                        ; Retourne le pointeur original
+    ret
