f5ec66839576618fb0ce607c7d1fc2a36033852c
matis <matis.taam@epitech.eu>
2025-02-17 20:32:47 +0100

    [ADD] strcspn function

--- src/strcspn.asm
+++ src/strcspn.asm
@@ -5,3 +5,38 @@
 ;; strcspn
 ;;
 
+BITS 64
+section .note.GNU-stack noexec
+
+section .text
+    global strcspn
+
+strcspn:
+    xor rcx, rcx                    ; Initialise le compteur à 0
+
+.outer_loop:
+    movzx eax, byte [rdi + rcx]     ; Charge le caractère actuel de la chaîne
+    test al, al                     ; Vérifie si fin de chaîne
+    jz .done
+
+    mov rdx, rsi                    ; Reset le pointeur de sous-chaîne
+
+.inner_loop:
+    movzx r8d, byte [rdx]           ; Charge le caractère de sous-chaîne
+    test r8b, r8b                   ; Vérifie si fin de sous-chaîne
+    jz .next_char
+
+    cmp al, r8b                     ; Compare les caractères
+    je .done
+
+    inc rdx                         ; Passe au caractère suivant dans sous-chaîne
+    jmp .inner_loop
+
+.next_char:
+    inc rcx                         ; Incrémente le compteur
+    jmp .outer_loop
+
+.done:
+    mov rax, rcx
+    ret
+
