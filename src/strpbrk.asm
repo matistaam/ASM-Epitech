c871280a6570192787051fa20b98950a7dfb4593
matis <matis.taam@epitech.eu>
2025-02-24 21:45:27 +0100

    [FIX] strpbrk

--- src/strpbrk.asm
+++ src/strpbrk.asm
@@ -9,29 +9,27 @@ BITS 64
 section .note.GNU-stack noexec
 
 section .text
-    global my_strpbrk
+    global strpbrk
 
-my_strpbrk:
+strpbrk:
     push rbp
     mov rbp, rsp
     push rdx
-    push r8
-    push r9
     mov rax, rdi                     ; Sauvegarde le pointeur de la chaîne
 
 .outer_loop:
-    movzx r9d, byte [rax]           ; Charge un caractère de la chaîne
-    test r9b, r9b                   ; Vérifie si fin de chaîne
+    mov ch, byte [rax]              ; Charge un caractère de la chaîne
+    test ch, ch                     ; Vérifie si fin de chaîne
     jz .not_found
 
     mov rdx, rsi                    ; Reset le pointeur de la sous-chaîne
 
 .inner_loop:
-    movzx r8d, byte [rdx]           ; Charge un caractère de la sous-chaîne
-    test r8b, r8b                   ; Vérifie si fin de la sous-chaîne
+    mov cl, byte [rdx]              ; Charge un caractère de la sous-chaîne
+    test cl, cl                     ; Vérifie si fin de la sous-chaîne
     jz .next_char
 
-    cmp r9b, r8b                    ; Compare les caractères
+    cmp ch, cl                      ; Compare les caractères
     je .found
 
     inc rdx                         ; Caractère suivant dans la sous-chaîne
@@ -45,8 +43,6 @@ my_strpbrk:
     xor rax, rax
 
 .found:
-    pop r9
-    pop r8
     pop rdx
     leave
     ret
